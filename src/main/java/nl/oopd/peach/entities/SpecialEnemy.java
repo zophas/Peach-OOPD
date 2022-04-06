7fbd2a3b116b633d1ad98141513966743093e889
Timoy van Balkom <timoy@live.nl>
2022-04-06 14:53:04 +0200

    master

--- src/main/java/nl/oopd/peach/entities/SpecialEnemy.java
+++ src/main/java/nl/oopd/peach/entities/SpecialEnemy.java
@@ -11,7 +11,7 @@ import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.media.SoundClip;
 import com.github.hanyaeger.api.scenes.SceneBorder;
 
-public class SpecialEnemy extends DynamicSpriteEntity implements IBehaviour,SceneBorderCrossingWatcher {
+public class SpecialEnemy extends NormalEnemy {
  
     private Size enemySize;
     private int newXPos = 1280;
@@ -30,11 +30,6 @@ public class SpecialEnemy extends DynamicSpriteEntity implements IBehaviour,Scen
     }
 
     @Override
-    public void doDamage(){
-
-    }
-
-    @Override
     public void notifyBoundaryCrossing(SceneBorder sceneBorder) {
         setAnchorLocationX(getSceneWidth());
     }
@@ -45,16 +40,13 @@ public class SpecialEnemy extends DynamicSpriteEntity implements IBehaviour,Scen
             //do nothing
         } else if (collider instanceof HealthBonus) {
             //do nothing
-        }
+        } else {
         Coordinate2D enemyPos = new Coordinate2D(newXPos, newYPos);
        setAnchorLocation(enemyPos);   
     }
+}
 
-    @Override
-    public void isDying() {
-        // TODO Auto-generated method stub
-        
-    }
+
   
 }
 
